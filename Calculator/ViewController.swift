8a75c0a8141784e704b02ec34e25cdc4cded3bb4
Karina Cardozo <cardozo.karina5@icloud.com>
2022-07-07 09:46:21 -0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Karina Cardozo on 07/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
